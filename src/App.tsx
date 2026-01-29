3c4d0a363a8c6343c47e469d9d4a3121b5b03039
SwakarsaDigital <swakarsadigital@gmail.com>
2026-01-29 12:37:45 +0700

    bug2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 
 // --- CONFIGURATION ---
 // Your Google Apps Script URL (Ensure it ends with /exec)
